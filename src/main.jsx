853e90dcf5be366a91fe71203172f0edb82e2a70
daviddiazh <daviddiazlds1221@gmail.com>
2022-08-16 15:50:57 -0500

    Making hook -> useCounter.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import { HooksApp } from './HooksApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HooksApp />
+    <CounterWithCustomHook />
   </React.StrictMode>
 )

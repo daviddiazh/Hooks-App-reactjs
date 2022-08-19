4a668ec47c8fa53373959e0f3f4c716d0b3d7c7b
daviddiazh <daviddiazlds1221@gmail.com>
2022-08-18 21:52:34 -0500

    Custom Hook Form created

---
+++ src/02-useEffect/CustomHookForm.jsx
@@ -0,0 +1,47 @@
+import { useForm } from "../hooks/useForm"
+
+export const CustomHookForm = () => {
+
+    const { formState, onInputChange, username, email, password } = useForm({
+        username: '',
+        email: '',
+        password: '',
+    });
+
+    //const { username, email, password } = formState;
+
+    return (
+        <>
+            <h1>Custom Hook Form</h1>
+            <hr />
+
+            <input 
+                type='text'
+                className='form-control'
+                placeholder='Username'
+                name='username'
+                value={username}
+                onChange={ onInputChange }
+            />
+
+            <input 
+                type='email'
+                className='form-control mt-2'
+                placeholder='Email'
+                name='email'
+                value={email}
+                onChange={ onInputChange }
+            />
+
+            <input 
+                type='password'
+                className='form-control mt-2'
+                placeholder='Password'
+                name='password'
+                value={password}
+                onChange={ onInputChange }
+            />
+
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
-import { SimpleForm } from './02-useEffect/SimpleForm'
+//import { SimpleForm } from './02-useEffect/SimpleForm'
+import { CustomHookForm } from './02-useEffect/CustomHookForm'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SimpleForm />
+    <CustomHookForm />
   </React.StrictMode>
 )

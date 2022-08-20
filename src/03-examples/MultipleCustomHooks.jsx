f984d09a79d9828ff868952e4bec1a760a061be1
daviddiazh <daviddiazlds1221@gmail.com>
2022-08-20 18:39:15 -0500

    Refactoring <MultipleCustomHooks /> component

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { useCounter } from '../hooks/useCounter';
-import { useFetch } from '../hooks/useFetch';
+import { LoadingQuote, Quote } from './';
+import { useCounter, useFetch } from '../hooks';
 
 export const MultipleCustomHooks = () => {
     
@@ -15,17 +14,8 @@ export const MultipleCustomHooks = () => {
 
             {
                 isLoading 
-                    ? (
-                        <div className='alert alert-info text-center'>
-                            Loading...
-                        </div>
-                    )
-                    : (
-                        <blockquote className='blockquote text-end'>
-                            <p className='mb-1'>{ quote }</p>
-                            <footer className='blockquote-footer'> { author }</footer>
-                        </blockquote>
-                    )
+                    ? <LoadingQuote /> 
+                    : <Quote quote={ quote } author={ author } />
             }
 
             <button 

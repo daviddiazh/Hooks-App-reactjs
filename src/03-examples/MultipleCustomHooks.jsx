5e52ad8328ae82c32b74c8a56669b32cfdfce770
daviddiazh <daviddiazlds1221@gmail.com>
2022-08-20 17:38:12 -0500

    Custom hook for fetch api

---
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useFetch } from '../hooks/useFetch';
+
+export const MultipleCustomHooks = () => {
+    
+    const { data, isLoading, hasError } = useFetch('https://www.breakingbadapi.com/api/quotes/1')
+    console.log(isLoading, {data})
+    return (
+        <>
+            <h1>BreakingBad Quotes</h1>
+            <hr />
+
+
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
-import { CustomHookForm } from './02-useEffect/CustomHookForm'
+//import { CustomHookForm } from './02-useEffect/CustomHookForm'
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CustomHookForm />
+    <MultipleCustomHooks />
   </React.StrictMode>
 )
